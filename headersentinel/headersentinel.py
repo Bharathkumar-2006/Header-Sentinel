871d1a5c32de619ecb4607e9b1eb84d8e0fa7487
Bharathkumar-2006 <bharathkumar50033@gmail.com>
2025-12-02 14:10:30 +0530

    removed proxy and file upload options

--- headersentinel/headersentinel.py
+++ headersentinel/headersentinel.py
@@ -443,12 +443,6 @@ def parse_options():
     parser.add_option("-k", "--deprecated", dest="show_deprecated", default=False,
                       help="Display deprecated headers",
                       action="store_true")
-    parser.add_option("--proxy", dest="proxy",
-                      help="Set a proxy (Ex: http://127.0.0.1:8080)",
-                      metavar="PROXY_URL")
-    parser.add_option("--hfile", dest="hfile",
-                      help="Load a list of hosts from a flat file",
-                      metavar="PATH_TO_FILE")
     (options, targets) = parser.parse_args()
 
     if len(targets) < 1 and options.hfile is None:
